2f72785a7d3c48d7f99bd6b8826b2af682082113
wilde <wilde0115@163.com>
2016-05-20 13:11:32 +0800

    add configure files so can configure back-end

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+  <vux></vux>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Vux from './components/Vux'
 
 export default {
   components: {
-    Hello
+    Hello,
+    Vux
   }
 }
 </script>
@@ -37,13 +19,13 @@ html {
   height: 100%;
 }
 
-body {
+/*body {
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
-}
-
+}*/
+/*
 #app {
   color: #2c3e50;
   margin-top: -100px;
@@ -60,5 +42,5 @@ body {
 .logo {
   width: 100px;
   height: 100px
-}
+}*/
 </style>
---
+++ src/components/Vux.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{msg}}:{{name}}</h1>
+  <div>
+    <group>
+      <cell title="vue" value="cool"></cell>
+      <cell :title="msg" :value="name"></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import Group from 'vux/components/group'
+import Cell from 'vux/components/cell'
+
+var Vue = require('vue')
+
+Vue.use(require('vue-resource'))
+
+export default {
+  data () {
+    return {
+      msg: 'hello',
+      name: 'andy'
+    }
+  },
+  ready: function () {
+    this.$http.jsonp('http://news-at.zhihu.com/api/4/start-image/1080*1776').then(function (response) {
+      console.log(response.ststus)
+    })
+  },
+  components: {
+    Group,
+    Cell
+  }
+}
+</script>
+
+<style>
+@import '~vux/vux.css';
+</style>
